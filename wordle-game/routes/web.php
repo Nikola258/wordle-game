2b15a81ae6273eea3c293b31440db08766201f64
Nikola258 <nikolic07nikola@gmail.com>
2025-05-07 23:55:00 +0200

    Dashboard en login pagina toegevoegd
    Ik heb 2 files toegevoegd: de dashboard pagina en de login pagina
    Verder heb ik voor beide pagina's een route toegevoegd
    
    De dashboard pagina is bedoeld voor gebruikers na inloggen.
    De login pagina zorgt ervoor dat gebruikers kunnen inloggen.

--- wordle-game/routes/web.php
+++ wordle-game/routes/web.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {return view('welcome');});
 Route::get('/home', function () {return view('home');});
 Route::get('/wordle', function () {return view('wordle');});
+Route::get('/login', function () {return view('login');});
+Route::get('/dashboard', function () {return view('dashboard');});
