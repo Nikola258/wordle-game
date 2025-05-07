a2832c5052825774334cea6f6b5efa7120363eaf
Nikola258 <nikolic07nikola@gmail.com>
2025-05-07 23:46:49 +0200

    first commit

---
+++ wordle-game/routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {return view('welcome');});
+Route::get('/home', function () {return view('home');});
+Route::get('/wordle', function () {return view('wordle');});
