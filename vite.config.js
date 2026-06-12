cc61933d71fd0632dfa80ceb6940f8a6bc4148bb
Nikola258 <nikolic07nikola@gmail.com>
2026-06-12 10:48:18 +0200

    Laravel Breeze installed for login and register

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            'alpinejs': fileURLToPath(new URL('./node_modules/alpinejs/dist/module.esm.js', import.meta.url)),
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
-            ],
         }),
-        tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
